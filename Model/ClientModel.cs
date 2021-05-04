848ef30b2df4f910217a2a6911e8a6d4bdfbbd6b
MarianaAntunesJ <marianajantunes97@gmail.com>
2021-05-04 13:33:01 -0300

    Models creation

---
+++ Model/ClientModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ToyStore.Model
+{
+    class ClientModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // Social Security Number is similar to CPF (Cadastro de pessoa física)
+        public string SSN { get; set; }
+
+        public DateTime Birthday { get; set; }
+        public string Cellphone { get; set; }
+        public char Gender { get; set; }
+        public bool Active { get; set; }
+
+        public ClientModel()
+        {
+        }
+
+        public ClientModel(int id, string name, string snn, DateTime birthday, string cellphone, char gender, bool active)
+        {
+            Id = id;
+            Name = name;
+            SSN = snn;
+            Birthday = birthday;
+            Cellphone = cellphone;
+            Gender = gender;
+            Active = active;
+        }
+    }
+}
---
+++ Model/ToyModel.cs
@@ -0,0 +1,22 @@
+﻿namespace ToyStore.Model
+{
+    class ToyModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AmountOfStock { get; set; }
+        public bool Active { get; set; }
+
+        public ToyModel()
+        {
+        }
+
+        public ToyModel(int id, string name, int amountOfStock, bool active)
+        {
+            Id = id;
+            Name = name;
+            AmountOfStock = amountOfStock;
+            Active = active;
+        }
+    }
+}
